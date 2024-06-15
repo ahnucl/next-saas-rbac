47018a54ac159fa49bd6f3d37afc3020606e7689
Leonardo Cunha <lalmcunha@gmail.com>
2024-06-15 15:25:55 -0300

    Integra form com action: validações e alerta

--- apps/web/src/app/auth/sign-in/actions.ts
+++ apps/web/src/app/auth/sign-in/actions.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { apiSingInWithEmailAndPassword } from '@/http/sign-in-with-email-and-password'
 
 /**
@@ -7,13 +10,46 @@ import { apiSingInWithEmailAndPassword } from '@/http/sign-in-with-email-and-pas
  *
  */
 
-export async function ssSingInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Please provide your password.' }),
+})
 
-  const result = await apiSingInWithEmailAndPassword({
-    email: email.toString(),
-    password: password.toString(),
-  })
+export async function ssSingInWithEmailAndPassword(_: unknown, data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
 
-  console.log(result)
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await apiSingInWithEmailAndPassword({
+      email: email.toString(),
+      password: password.toString(),
+    })
+
+    console.log(token)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
