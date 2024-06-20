484c286dc686f2a2e31b09e5a3017d64b47bf90e
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-06-20 09:19:36 -0300

    Cria autenticação

--- apps/web/src/auth/index.ts
+++ apps/web/src/auth/index.ts
@@ -1,5 +1,27 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { apiGetProfile } from '@/http/get-profile'
 
 export function isAuthenticated() {
   return !!cookies().get('token')?.value
+}
+
+// use server only
+export async function auth() {
+  const token = cookies().get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await apiGetProfile()
+
+    return { user }
+  } catch {
+    // cookies().delete('token')
+  }
+
+  redirect('/auth/sign-out')
 }
