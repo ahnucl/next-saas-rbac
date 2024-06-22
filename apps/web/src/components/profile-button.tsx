f6f8113e37ea44498b61b275f0e5d58e44a780fc
Leonardo Cunha <lalmcunha@gmail.com>
2024-06-22 15:37:48 -0300

    Cria header

---
+++ apps/web/src/components/profile-button.tsx
@@ -0,0 +1,58 @@
+import { ChevronDown, LogOut } from 'lucide-react'
+
+import { auth } from '@/auth'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+function getInitials(name: string): string {
+  return name
+    .split(' ')
+    .filter((part) => part.length > 0)
+    .slice(0, 2)
+    .map((part) => part[0].toUpperCase())
+    .join('')
+}
+
+export async function ProfileButton() {
+  const { user } = await auth()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center gap-3 outline-none">
+        <div className="flex flex-col items-end">
+          <span className="text-sm font-medium">{user.name}</span>
+          <span className="text-xs text-muted-foreground">{user.email}</span>
+        </div>
+
+        <Avatar>
+          {user.avatarUrl && <AvatarImage src={user.avatarUrl} />}
+          {user.name && (
+            <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+          )}
+        </Avatar>
+        <ChevronDown className="size-4 text-muted-foreground" />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem asChild>
+          <a href="/api/auth/sign-out">
+            <LogOut className="mr-2 size-4" />
+            Sign Out
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+/**
+ * Sempre que for necessário redirecionar para um link externo ou um route handler
+ * (qualquer coisa não uma página do app) é melhor usar a âncora pois o Link presente
+ * no viewport do usuário faz o Next realizar um pre-fetch
+ */
