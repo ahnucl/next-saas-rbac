023c4c879cb33cfa7fbdc4500744e6410fa5f633
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-06-25 09:25:26 -0300

    Corrige rota de signout

--- apps/web/src/app/api/auth/sign-out/route.ts
+++ apps/web/src/app/api/auth/sign-out/route.ts
@@ -4,7 +4,7 @@ import { type NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
   const redirectURL = request.nextUrl.clone()
 
-  redirectURL.password = '/auth/sign-in'
+  redirectURL.pathname = '/auth/sign-in'
   redirectURL.search = ''
 
   cookies().delete('token')
