8d993e422b67ea95908a0c2c5760807c828dc2b7
Leonardo Cunha <lalmcunha@gmail.com>
2024-06-29 13:15:18 -0300

    Ajusta variáveis ambiente

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,14 +1,15 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function ssSignInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
-
-  githubSignInURL.searchParams.set('client_id', 'Ov23lirUO0uY4wKPxUye')
+  // 'Ov23lirUO0uY4wKPxUye'
+  githubSignInURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubSignInURL.searchParams.set(
     'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
   )
   githubSignInURL.searchParams.set('scope', 'user')
 
