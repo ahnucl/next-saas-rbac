5ab6dc0a4cf4ac243eb2d97babed05cd0c3f8dac
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-26 10:16:37 -0300

    Finaliza criação de projetos

---
+++ apps/web/src/app/(app)/org/[slug]/create-project/actions.ts
@@ -0,0 +1,52 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+import { getCurrentOrg } from '@/auth'
+import { apiCreateProject } from '@/http/create-project'
+
+const createProjectSchema = z.object({
+  name: z.string().min(4, { message: 'Please include at least 4 characters.' }),
+  description: z.string(),
+})
+
+export async function ssCreateProject(data: FormData) {
+  const result = createProjectSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { name, description } = result.data
+
+  try {
+    await apiCreateProject({
+      org: getCurrentOrg()!, // A página que chama essa função não pode ter org nulo (contexto)
+      name,
+      description,
+    })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the project!',
+    errors: null,
+  }
+}
