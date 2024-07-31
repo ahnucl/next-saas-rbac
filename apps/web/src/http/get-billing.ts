2cc9dfef37bf155e80d17e903876141c0e88e67c
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-07-31 14:52:12 -0300

    Tabela de faturamento

---
+++ apps/web/src/http/get-billing.ts
@@ -0,0 +1,25 @@
+import { api } from './api-client'
+
+interface GetBillingResponse {
+  billing: {
+    seats: {
+      amount: number
+      unit: number
+      price: number
+    }
+    projects: {
+      amount: number
+      unit: number
+      price: number
+    }
+    total: number
+  }
+}
+
+export async function apiGetBilling(orgSlug: string) {
+  const result = await api
+    .get(`organizations/${orgSlug}/billing`)
+    .json<GetBillingResponse>()
+
+  return result
+}
